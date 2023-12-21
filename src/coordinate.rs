1db30a427a14533d01c18de02e9e2af7d20ad951
SamYouatt <sdyouatt@gmail.com>
2023-12-21 08:30:19 +0000

    add method to move coord in direction

--- src/coordinate.rs
+++ src/coordinate.rs
@@ -1,3 +1,5 @@
+use crate::direction::Direction;
+
 pub struct Coordinate {
     pub x: isize,
     pub y: isize,
@@ -6,5 +8,14 @@ pub struct Coordinate {
 impl Coordinate {
     pub fn new(x: isize, y: isize) -> Coordinate {
         Coordinate { x, y }
+    }
+
+    pub fn move_dir(&self, dir: &Direction) -> Coordinate {
+        let offset = dir.get_offset();
+
+        Coordinate {
+            x: self.x + offset.0,
+            y: self.y + offset.0,
+        }
     }
 }
